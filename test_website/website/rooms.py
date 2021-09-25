f537873ecdff94b695b1cd60c24ff15112b2eafc
Swordlord12 <89358651+Swordlord12@users.noreply.github.com>
2021-09-25 18:00:35 -0400

    Add files via upload

--- test_website/website/rooms.py
+++ test_website/website/rooms.py
@@ -1,17 +1,40 @@
-from flask import Blueprint, render_template, request, flash, url_for, redirect
+from flask import Blueprint, render_template, request, flash, url_for, redirect, jsonify
 from flask_login import login_required, current_user
-from .models import Game, GamesJoined, User
-import random
-import string
+from .models import Game, GamesJoined, User, Empires
 from . import db
+import sqlite3 as dbapi
+import json
 
 rooms = Blueprint('rooms', __name__)
 
-@rooms.route('<int:game_id>')
+@rooms.route('<int:game_id>', methods=["GET", "POST"])
 @login_required
 def room(game_id):
+    if request.method == "POST":
+        print("worked thus far")
+        empire = request.form.get('empire')
+        empire_query = Empires.query.filter_by(name=empire)
+        if empire_query == empire:
+            flash('That empire name is in use.', category='error')
+        if len(empire) < 1:
+            flash('You cannot leave the empire name blank.')
+        if len(empire) > 200:
+            flash('The max empire name length is 199 characters.')
+        else:
+            print("Reached this far")
+            new_empire = Empires(name=empire, user=current_user.id, game=game_id)
+            empire_query = Empires.query.filter_by(game=game_id, user=current_user.id).first()
+            if empire_query:
+                db.session.delete(Empires.query.get(empire_query.id))
+                db.session.commit()
+                db.session.add(new_empire)
+                db.session.commit()
+            else:
+                db.session.add(new_empire)
+                db.session.commit()
     players = []
     players_output = []
+    empires = {}
     id = game_id
     for games in db.session.query(Game).filter_by(id = game_id):
         name = games.game_name
@@ -20,7 +43,16 @@ def room(game_id):
     for player in players:
         for result in db.session.query(User).filter_by(id=player):
             players_output.append(result)
-    return render_template("room.html", user=current_user, players = players_output, game = name, game_id = id)
+    for empire in db.session.query(Empires).filter_by(game=id):
+        empires[f"{empire.user}"]=empire.name
+        print(empire.name)
+    empire_key = [empires]
+    return render_template("room.html", user=current_user, players = players_output, game = name, game_id = id, empire_key=empires)
+
+@rooms.route('<int:game_id>/<empire>', methods=['PUT'])
+def update_empire(game_id, empire):
+    pass
+
 
 @rooms.route('<int:game_id>/map')
 @login_required
