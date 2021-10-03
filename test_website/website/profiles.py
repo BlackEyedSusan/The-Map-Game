219d2c8cb8b32254e939a38fd025248bc3d108ad
Swordlord12 <89358651+Swordlord12@users.noreply.github.com>
2021-10-03 19:45:44 -0400

    Add files via upload

--- test_website/website/profiles.py
+++ test_website/website/profiles.py
@@ -2,8 +2,6 @@ from flask import Blueprint, render_template, request, flash, url_for, redirect,
 from flask_login import login_required, current_user
 from .models import Game, GamesJoined, User, Empires, Friends
 from . import db
-import sqlite3 as dbapi
-import json
 
 profiles = Blueprint('profiles', __name__)
 
@@ -23,7 +21,7 @@ def profile(user_id):
             else:
                 db.session.add(new_friend)
                 db.session.commit()
-                flash('User addded to friends!', category='error')
+                flash('User addded to friends!', category='success')
                 redirect(url_for('profiles.profile', user_id=user_id))
         elif request.form.get("Remove Friend") == "Remove Friend":
             is_friends1 = Friends.query.filter_by(user1=user_id, user2=current_user.id).first()
@@ -33,7 +31,7 @@ def profile(user_id):
             if is_friends2 != None:
                 db.session.delete(is_friends2)
             db.session.commit()
-            flash('User removed from friends.', category='success')
+            flash('User removed from friends.', category='neutral')
             redirect(url_for('profiles.profile', user_id=user_id))
             
     user = User.query.filter_by(id=user_id).first()
@@ -50,3 +48,17 @@ def profile(user_id):
     else:
         is_current_user = False
     return render_template('profile.html', is_current=is_current_user, user=current_user, player=user, games_joined=counter, is_friends=is_friends)
+
+@login_required
+@profiles.route('friends')
+def friends():
+    result_list = []
+    friends_list = []
+    for friend in db.session.query(Friends).filter_by(user1=current_user.id):
+        friends_list.append(friend.user2)
+    for friend in db.session.query(Friends).filter_by(user2=current_user.id):
+        friends_list.append(friend.user1)
+    for friend in friends_list:
+        result = db.session.query(User).filter_by(id=friend.id).first()
+        result_list.append(result)
+    return render_template('friends.html', user=current_user, friends=result_list)
--- test_website/website/static/home.js
+++ test_website/website/static/home.js
@@ -1,6 +1,1 @@
-var h1Elements = document.getElementsByTagName("a");
-for(var i = 6; i < h1Elements.length; i++) {
-   h1Elements[i].style.color = "black";
-   console.log('done this tick of the for loop')
-}
-document.getElementsByName("game_list").style.color = "black"
+//no its not empty git
